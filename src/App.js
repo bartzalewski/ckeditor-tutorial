84eacac2e52e750495b2e7f2a4e058b6204a6a4d
Bart Zalewski <me@bartzalewski.com>
2020-08-26 00:05:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState('');
+  const handleChange = (e, editor) => setValue(editor.getData());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4 w-50">
+      <CKEditor editor={ClassicEditor} onChange={handleChange} />
+      <h4 className="mt-4">Your output</h4>
+      <div className="card p-2 mt-4">
+        <span>{value}</span>
+      </div>
     </div>
   );
 }
-
-export default App;
